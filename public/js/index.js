9c1d1e29b24ebd7add14329380b5a68e423d1954
James Stack <james.a.stack@gmail.com>
2017-11-21 21:55:31 -0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', () => {
   console.log('Connected to server');
-  socket.emit('createMessage', {
-    from: 'jen@example.com',
-    text: 'Hey. This is James'
-  })
 })
 
 socket.on('disconnect', () => {
