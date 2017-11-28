a1c1947fce588cf2233a29c97872f2eb3a6dfbac
James Stack <james.a.stack@gmail.com>
2017-11-28 15:25:19 -0800

    Add formattedTime to messages

---
+++ playground/time.js
@@ -0,0 +1,14 @@
+var moment = require('moment');
+// Jan 1st 1970 00:00:00 am
+
+
+// var date = new Date();
+//
+// console.log(date.getMonth());
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+var createdAt = 1234
+var date = moment(createdAt);
+console.log(date.format('h:mm a'))
+
+// 3:11 pm
--- public/js/index.js
+++ public/js/index.js
@@ -9,17 +9,19 @@ socket.on('disconnect', () => {
 })
 
 socket.on('newMessage', (message) => {
-  console.log('newMessage', message);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
   jQuery('#messages').append(li);
 })
 
 socket.on('newLocationMessage', (message) => {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
@@ -28,11 +30,13 @@ socket.on('newLocationMessage', (message) => {
 jQuery('#message-form').on('submit', (e) => {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, () => {
-
+    messageTextbox.val('')
   })
 })
 
@@ -42,12 +46,16 @@ locationButton.on('click', () => {
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition((position) => {
+    locationButton.removeAttr('disabled').text('Send location')
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, () => {
+    locationButton.removeAttr('disabled').text('Send location')
     alert('Unable to fetch location')
   })
 })
