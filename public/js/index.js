59be7686a0812783a7ef1b51326204222412131b
James Stack <james.a.stack@gmail.com>
2017-11-29 12:59:19 -0800

    Add scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom (){
+  // Selectors
+  var messages = jQuery('#messages')
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+
+}
+
 socket.on('connect', () => {
   console.log('Connected to server');
 })
@@ -18,6 +35,7 @@ socket.on('newMessage', (message) => {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 })
 
 socket.on('newLocationMessage', (message) => {
@@ -29,6 +47,7 @@ socket.on('newLocationMessage', (message) => {
     url: message.url
   })
   jQuery('#messages').append(html);
+  scrollToBottom();
 })
 
 jQuery('#message-form').on('submit', (e) => {
